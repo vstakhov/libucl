a45535648289a0005e17a9bc503a5c69b6bb28c6
Vsevolod Stakhov <vsevolod@highsecure.ru>
2013-10-11 14:40:30 +0100

    Some tunes to chartable.

--- utils/chargen.c
+++ utils/chargen.c
@@ -70,7 +70,7 @@ main (int argc, char **argv)
 		if (isalnum (i) || i == '-' || i == '_' || i >= 0x80) {
 			r += print_flag ("UCL_CHARACTER_KEY", &need_or, valbuf + r);
 		}
-		if (i == 0 || i == '\r' || i == '\n' || i == '{' || i == '[' || i == ';' || i == '/' || i == '#') {
+		if (i == 0 || i == '\r' || i == '\n' || i == '{' || i == '[' || i == ';' || i == '#') {
 			r += print_flag ("UCL_CHARACTER_VALUE_END", &need_or, valbuf + r);
 		}
 		else {
@@ -89,7 +89,7 @@ main (int argc, char **argv)
 			r += print_flag ("UCL_CHARACTER_DENIED", &need_or, valbuf + r);
 		}
 
-		if (isalnum (i)) {
+		if (isprint (i)) {
 			r += sprintf (valbuf + r, " /* %c */", i);
 		}
 		if (i != 254) {
