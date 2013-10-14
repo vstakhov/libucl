5f8c5db7885d74df5701161fb4992ecdcf58db48
Vsevolod Stakhov <vsevolod@highsecure.ru>
2013-10-14 13:44:12 +0100

    Load all tests from the directory.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
-TESTS="1 2 3"
-
 if [ $# -lt 1 ] ; then
 	echo 'Specify binary to run as the first argument'
 	exit 1
 fi
 
-for _t in $TESTS ; do
-	$1 ${TEST_DIR}/$_t.in ${TEST_DIR}/$_t.out
+
+for _tin in ${TEST_DIR}/*.in ; do
+	_t=`echo $_tin | sed -e 's/.in$//'`
+	$1 $_t.in $_t.out
 	if [ $? -ne 0 ] ; then
-		rm ${TEST_DIR}/$_t.out
+		rm $_t.out
 		echo "Test: $_t failed"
 		exit 1
 	fi
-	if [ -f ${TEST_DIR}/$_t.res ] ; then
-	diff -s ${TEST_DIR}/$_t.out ${TEST_DIR}/$_t.res -u 2>/dev/null
+	if [ -f $_t.res ] ; then
+	diff -s $_t.out $_t.res -u 2>/dev/null
 		if [ $? -ne 0 ] ; then
-			rm ${TEST_DIR}/$_t.out
+			rm $_t.out
 			echo "Test: $_t output missmatch"
 			exit 1
 		fi
 	fi
-	rm ${TEST_DIR}/$_t.out
+	rm $_t.out
 done
