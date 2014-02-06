5c30c1d8cbab35b359caac94bcaa259c78354fba
Vsevolod Stakhov <vsevolod@highsecure.ru>
2014-02-06 14:20:48 +0000

    Initialize variable to silence warning.

--- src/ucl_parser.c
+++ src/ucl_parser.c
@@ -901,7 +901,7 @@ static bool
 ucl_parse_key (struct ucl_parser *parser, struct ucl_chunk *chunk, bool *next_key, bool *end_of_object)
 {
 	const unsigned char *p, *c = NULL, *end, *t;
-	const char *key;
+	const char *key = NULL;
 	bool got_quote = false, got_eq = false, got_semicolon = false,
 			need_unescape = false, ucl_escape = false, var_expand = false,
 			got_content = false, got_sep = false;
