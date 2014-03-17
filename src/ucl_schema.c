b011cc9bb3d7e979b85f52629cc4c6373eece3ef
Vsevolod Stakhov <vsevolod@highsecure.ru>
2014-03-17 14:15:47 +0000

    Add validator for UCL_OBJECT type.

--- src/ucl_schema.c
+++ src/ucl_schema.c
@@ -91,6 +91,9 @@ ucl_object_type_to_string (ucl_type_t type)
 	return res;
 }
 
+/*
+ * Create validation error
+ */
 static void
 ucl_schema_create_error (struct ucl_schema_error *err,
 		enum ucl_schema_error_code code, ucl_object_t *obj,
@@ -107,39 +110,190 @@ ucl_schema_create_error (struct ucl_schema_error *err,
 	}
 }
 
+/*
+ * Validate object
+ */
+static bool
+ucl_schema_validate_object (ucl_object_t *schema,
+		ucl_object_t *obj, struct ucl_schema_error *err)
+{
+	ucl_object_t *elt, *prop, *found, *additional_schema = NULL,
+			*required = NULL;
+	ucl_object_iter_t iter = NULL, piter = NULL;
+	bool ret = true, allow_additional = true;
+	int64_t minmax;
+
+	while (ret && (elt = ucl_iterate_object (schema, &iter, true)) != NULL) {
+		if (elt->type == UCL_OBJECT &&
+				strcmp (ucl_object_key (elt), "properties") == 0) {
+			while (ret && (prop = ucl_iterate_object (elt, &piter, true)) != NULL) {
+				found = ucl_object_find_key (obj, ucl_object_key (prop));
+				if (found) {
+					ret = ucl_object_validate (prop, found, err);
+				}
+			}
+		}
+		else if (strcmp (ucl_object_key (elt), "additionalProperties") == 0) {
+			if (elt->type == UCL_BOOLEAN) {
+				if (!ucl_object_toboolean (elt)) {
+					/* Deny additional fields completely */
+					allow_additional = false;
+				}
+			}
+			else if (elt->type == UCL_OBJECT) {
+				/* Define validator for additional fields */
+				additional_schema = elt;
+			}
+			else {
+				ucl_schema_create_error (err, UCL_SCHEMA_INVALID_SCHEMA, elt,
+						"additionalProperties attribute is invalid in schema");
+				ret = false;
+			}
+		}
+		else if (strcmp (ucl_object_key (elt), "required") == 0) {
+			if (elt->type == UCL_ARRAY) {
+				required = elt;
+			}
+			else {
+				ucl_schema_create_error (err, UCL_SCHEMA_INVALID_SCHEMA, elt,
+						"required attribute is invalid in schema");
+				ret = false;
+			}
+		}
+		else if (strcmp (ucl_object_key (elt), "minProperties") == 0
+				&& ucl_object_toint_safe (elt, &minmax)) {
+			if (obj->len < minmax) {
+				ucl_schema_create_error (err, UCL_SCHEMA_CONSTRAINT, obj,
+						"object has not enough properties: %u, minimum is: %u",
+						obj->len, (unsigned)minmax);
+				ret = false;
+			}
+		}
+		else if (strcmp (ucl_object_key (elt), "maxProperties") == 0
+				&& ucl_object_toint_safe (elt, &minmax)) {
+			if (obj->len > minmax) {
+				ucl_schema_create_error (err, UCL_SCHEMA_CONSTRAINT, obj,
+						"object has too many properties: %u, maximum is: %u",
+						obj->len, (unsigned)minmax);
+				ret = false;
+			}
+		}
+		/* Ignore unknown keywords in schema for now */
+	}
+
+	if (ret) {
+		/* Additional properties */
+		if (!allow_additional || additional_schema != NULL) {
+			/* Check if we have exactly the same properties in schema and object */
+			iter = NULL;
+			prop = ucl_object_find_key (schema, "properties");
+			while ((elt = ucl_iterate_object (obj, &iter, true)) != NULL) {
+				if (prop == NULL ||
+					(found = ucl_object_find_key (prop, ucl_object_key (elt))) == NULL) {
+					if (!allow_additional) {
+						ucl_schema_create_error (err, UCL_SCHEMA_CONSTRAINT, obj,
+								"object has undefined property %s",
+								ucl_object_key (elt));
+						return false;
+					}
+					else if (additional_schema != NULL) {
+						if (!ucl_object_validate (additional_schema, elt, err)) {
+							return false;
+						}
+					}
+				}
+			}
+		}
+		/* Required properties */
+		if (required != NULL) {
+			iter = NULL;
+			while ((elt = ucl_iterate_object (required, &iter, true)) != NULL) {
+				if (ucl_object_find_key (obj, ucl_object_key (elt)) == NULL) {
+					ucl_schema_create_error (err, UCL_SCHEMA_MISSING_PROPERTY, obj,
+							"object has missing property %s",
+							ucl_object_key (elt));
+					return false;
+				}
+			}
+		}
+	}
+
+
+	return ret;
+}
+
+/*
+ * Returns whether this object is allowed for this type
+ */
+static bool
+ucl_schema_type_is_allowed (ucl_object_t *type, ucl_object_t *obj,
+		struct ucl_schema_error *err)
+{
+	ucl_object_iter_t iter = NULL;
+	ucl_object_t *elt;
+	const char *type_str;
+	ucl_type_t t;
+
+	if (type == NULL) {
+		/* Any type is allowed */
+		return true;
+	}
+
+	if (type->type == UCL_ARRAY) {
+		/* One of allowed types */
+		while ((elt = ucl_iterate_object (type, &iter, true)) != NULL) {
+			if (ucl_schema_type_is_allowed (elt, obj, err)) {
+				return true;
+			}
+		}
+	}
+	else if (type->type == UCL_STRING) {
+		type_str = ucl_object_tostring (type);
+		if (!ucl_string_to_type (type_str, &t)) {
+			ucl_schema_create_error (err, UCL_SCHEMA_INVALID_SCHEMA, type,
+					"Type attribute is invalid in schema");
+			return false;
+		}
+		if (obj->type != t) {
+			/* Some types are actually compatible */
+			if (obj->type == UCL_TIME && t == UCL_FLOAT) {
+				return true;
+			}
+			else if (obj->type == UCL_INT && t == UCL_FLOAT) {
+				return true;
+			}
+			else {
+				ucl_schema_create_error (err, UCL_SCHEMA_TYPE_MISMATCH, obj,
+						"Invalid type of %s, expected %s",
+						ucl_object_type_to_string (obj->type),
+						t);
+			}
+		}
+		else {
+			/* Types are equal */
+			return true;
+		}
+	}
+
+	return false;
+}
+
 bool
 ucl_object_validate (ucl_object_t *schema,
 		ucl_object_t *obj, struct ucl_schema_error *err)
 {
-	ucl_type_t type;
 	ucl_object_t *elt;
-	const char *type_str;
 
 	elt = ucl_object_find_key (schema, "type");
 
-	/* Allow exactly one type attribute */
-	if (elt == NULL || elt->next != NULL ||
-			!ucl_object_tostring_safe (elt, &type_str) ||
-			!ucl_string_to_type (type_str, &type)) {
-		ucl_schema_create_error (err, UCL_SCHEMA_INVALID_SCHEMA, schema,
-				"Type attribute is invalid in schema");
+	if (!ucl_schema_type_is_allowed (elt, obj, err)) {
 		return false;
 	}
 
-	if (obj->type != type) {
-		/* Some types are actually compatible */
-		if (obj->type != UCL_TIME && type != UCL_FLOAT) {
-			ucl_schema_create_error (err, UCL_SCHEMA_TYPE_MISMATCH, obj,
-					"Invalid type of %s, expected %s",
-					ucl_object_type_to_string (obj->type),
-					type);
-			return false;
-		}
-	}
-
 	/* XXX: to implement */
-	switch (type) {
+	switch (obj->type) {
 	case UCL_OBJECT:
+		return ucl_schema_validate_object (schema, obj, err);
 		break;
 	case UCL_ARRAY:
 		break;
