95f6e9ab20f115ec368fa49097eb7ee6c9655436
Vsevolod Stakhov <vsevolod@highsecure.ru>
2015-06-03 16:28:51 +0100

    Add msgpack emitter type.

--- src/ucl_emitter.c
+++ src/ucl_emitter.c
@@ -62,6 +62,7 @@ UCL_EMIT_TYPE_OPS(json);
 UCL_EMIT_TYPE_OPS(json_compact);
 UCL_EMIT_TYPE_OPS(config);
 UCL_EMIT_TYPE_OPS(yaml);
+UCL_EMIT_TYPE_OPS(msgpack);
 
 #define UCL_EMIT_TYPE_CONTENT(type) {	\
 	.ucl_emitter_write_elt = ucl_emit_ ## type ## _elt,	\
@@ -71,12 +72,12 @@ UCL_EMIT_TYPE_OPS(yaml);
 	.ucl_emitter_end_array = ucl_emit_ ## type ##_end_array	\
 }
 
-
 const struct ucl_emitter_operations ucl_standartd_emitter_ops[] = {
 	[UCL_EMIT_JSON] = UCL_EMIT_TYPE_CONTENT(json),
 	[UCL_EMIT_JSON_COMPACT] = UCL_EMIT_TYPE_CONTENT(json_compact),
 	[UCL_EMIT_CONFIG] = UCL_EMIT_TYPE_CONTENT(config),
-	[UCL_EMIT_YAML] = UCL_EMIT_TYPE_CONTENT(yaml)
+	[UCL_EMIT_YAML] = UCL_EMIT_TYPE_CONTENT(yaml),
+	[UCL_EMIT_MSGPACK] = UCL_EMIT_TYPE_CONTENT(msgpack)
 };
 
 /*
@@ -468,6 +469,41 @@ UCL_EMIT_TYPE_IMPL(json, false)
 UCL_EMIT_TYPE_IMPL(json_compact, true)
 UCL_EMIT_TYPE_IMPL(config, false)
 UCL_EMIT_TYPE_IMPL(yaml, false)
+
+static void
+ucl_emit_msgpack_elt (struct ucl_emitter_context *ctx,
+		const ucl_object_t *obj, bool first, bool print_key)
+{
+
+}
+
+static void
+ucl_emit_msgpack_start_obj (struct ucl_emitter_context *ctx,
+		const ucl_object_t *obj, bool print_key)
+{
+
+}
+
+static void
+ucl_emit_msgpack_start_array (struct ucl_emitter_context *ctx,
+		const ucl_object_t *obj, bool print_key)
+{
+
+}
+
+static void
+ucl_emit_msgpack_end_object (struct ucl_emitter_context *ctx,
+		const ucl_object_t *obj)
+{
+
+}
+
+static void
+ucl_emit_msgpack_end_array (struct ucl_emitter_context *ctx,
+		const ucl_object_t *obj)
+{
+
+}
 
 unsigned char *
 ucl_object_emit (const ucl_object_t *obj, enum ucl_emitter emit_type)
