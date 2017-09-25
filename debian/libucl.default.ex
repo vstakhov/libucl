4036663d8a5ab2e72f2e25f7101f7009f15ac6f4
Igor Galić <igalic@brainsware.at>
2017-09-25 16:56:06 +0200

    add debian/ build for libucl
    This should enable (outside contributors) to easily build their own .deb
    packages of this repository, or automate that process thru Launchpad's
    PPA service.
    
    This first commit is autogenerated thru `dh_make`.

---
+++ debian/libucl.default.ex
@@ -0,0 +1,10 @@
+# Defaults for libucl initscript
+# sourced by /etc/init.d/libucl
+# installed at /etc/default/libucl by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(libucl):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="libucl" command="/usr/bin/libucl"
