a96b76edd1269df30bc6756f57aa5a0cf6fd55c1
Anna Stakhova <anna@rspamd.com>
2018-03-18 13:45:47 +0000

    Fix windows build

--- src/ucl_internal.h
+++ src/ucl_internal.h
@@ -63,7 +63,9 @@
 #include <sys/stat.h>
 #endif
 #ifdef HAVE_SYS_PARAM_H
-#include <sys/param.h>
+# ifndef _WIN32
+# include <sys/param.h>
+# endif
 #endif
 
 #ifdef HAVE_LIMITS_H
@@ -76,7 +78,9 @@
 #include <errno.h>
 #endif
 #ifdef HAVE_UNISTD_H
-#include <unistd.h>
+# ifndef _WIN32
+# include <unistd.h>
+# endif 
 #endif
 #ifdef HAVE_CTYPE_H
 #include <ctype.h>
@@ -89,6 +93,31 @@
 #endif
 #ifdef HAVE_STRINGS_H
 #include <strings.h>
+#endif
+
+#if defined(_MSC_VER)
+/* Windows hacks */
+#include <BaseTsd.h>
+typedef SSIZE_T ssize_t;
+#define strdup _strdup
+#define snprintf _snprintf 
+#define vsnprintf _vsnprintf 
+#define strcasecmp _stricmp 
+#define strncasecmp _strnicmp
+#define S_ISREG(m) (((m) & S_IFMT) == S_IFREG)
+#define S_ISDIR(m) (((m) & S_IFMT) == S_IFDIR)
+#if _MSC_VER >= 1900
+#include <../ucrt/stdlib.h>
+#else
+#include <../include/stdlib.h>
+#endif
+#ifndef PATH_MAX
+#define PATH_MAX _MAX_PATH
+#endif
+
+/* Dirname, basename implementations */
+
+
 #endif
 
 #include "utlist.h"
