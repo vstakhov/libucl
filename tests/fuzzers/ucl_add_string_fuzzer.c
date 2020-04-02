b1bec3abf16c8ed2eefaf5e554bd9201450f5367
Adam Korczynski <adam@adalogics.com>
2020-04-02 22:03:13 +0100

    Removed null-terminator for input data

--- tests/fuzzers/ucl_add_string_fuzzer.c
+++ tests/fuzzers/ucl_add_string_fuzzer.c
@@ -4,17 +4,12 @@
 #include "ucl.h"
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-	char *ss = (char*)malloc(size+1);
-	memcpy(ss, data, size);
-	ss[size] = '\0';
        
 	struct ucl_parser *parser;
        	parser = ucl_parser_new(0);
 
-	ucl_parser_add_string(parser, ss, 0);
+	ucl_parser_add_string(parser, (char *)data, size);
 	
-	free(ss);
-
 	if (ucl_parser_get_error(parser) != NULL) {
 		return 0;
 	}
